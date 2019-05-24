7617736a731ec6dd01b637282cd4fb652222f4b8
Tumble <tumblegamer@gmail.com>
2019-05-24 17:59:55 +0100

    img list

--- src/popup/js/createtheme.js
+++ src/popup/js/createtheme.js
@@ -20,11 +20,11 @@ function normaliseFileName(text) {
     return text.split(" ").join("");
 }
 
-function getFormData(removeEmpty=false) {
+function getFormData(removeEmpty = false) {
     var formData = new FormData(createtpform);
     var data = {};
     formData.forEach(function (value, key) {
-        if((value&&value!=="")||!removeEmpty) data[key] = value;
+        if ((value && value !== "") || !removeEmpty) data[key] = value;
     });
     data.date = Date.now();
     data.packVersion = "0.1";
@@ -49,15 +49,15 @@ function createTPJSON() {
      */
     var textarea = document.querySelector('textarea#tpdata');
     var data = getFormData();
-    var url = dataurl + encodeURIComponent(JSON.stringify(data,0,2));
+    var url = dataurl + encodeURIComponent(JSON.stringify(data, 0, 2));
     var downloadLink = document.createElement("a");
     var filename = data.name || "texturepack";
     filename = normaliseFileName(filename);
-    downloadLink.setAttribute("href",url);
+    downloadLink.setAttribute("href", url);
     downloadLink.setAttribute("download", filename + ".bctp.json");
     downloadLink.click();
 
-    textarea.value = JSON.stringify(data,0,2);
+    textarea.value = JSON.stringify(data, 0, 2);
     return false;
 }
 
@@ -75,7 +75,26 @@ document.addEventListener('DOMContentLoaded', () => {
         var currentVersion = formats.texturePack.length - 1;
         createtpform.appendChild(getVersionIndicator(currentVersion))
         formats = formats.texturePack[currentVersion];
-        formats.forEach((f,fid) => {
+
+        /*
+        <div class="tp-attrib card">
+            <div class="row no-gutters">
+                <div class="col-2">
+                    <img src="https://dummyimage.com/160" class="card-img" alt="">
+                </div>
+                <div class="col-lg">
+                    <div class="card-header">
+                        <span>Name</span>
+                    </div>
+                    <div class="card-body">
+                        <input name="name" class="form-control px-2" required="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        */
+
+        formats.forEach((f, fid) => {
             var formItem;
             if (f.hidden) {
                 formItem = document.createElement("input");
@@ -84,7 +103,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
             }
             if (!f.hidden) {
+                formItem = document.createElement('div');
+                formItem.classList.add("tp-attrib", "card");
+                var row = document.createElement('div');
+                row.classList.add("row", "no-gutters");
+                formItem.appendChild(row);
+                if (f.category !== "info") {
+                    if (f.default) {
+                        var imgcol = document.createElement('div');
+                        imgcol.classList.add("col-2");
+                        var img = new Image(160);
+                        img.classList.add('card-img');
+                        getFileURL(f.default).then((url) => {
+                            img.setAttribute('src', url);
+                        });
+                        imgcol.appendChild(img);
+                        row.appendChild(imgcol);
+                    }
+                }
+                var infocol = document.createElement('div');
+                infocol.classList.add("col-lg");
+                var header = document.createElement('div');
+                header.classList.add('card-header');
+                var formItemLabel = document.createElement('span');
+                formItemLabel.innerHTML = f.label||f.name;
+                header.appendChild(formItemLabel);
 
+                infocol.appendChild(header);
+                row.appendChild(infocol);
+
+
+                /***************************************** *//*
                 formItem = document.createElement('div');
                 var formItemLabel = document.createElement('span');
                 var formItemValue = document.createElement('input');
@@ -101,30 +150,33 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
 
                 formItem.appendChild(formItemLabel);
-                formItem.appendChild(formItemValue);
-                
+                formItem.appendChild(formItemValue);*/
+
+
+
+
             }
 
-            if(f.category){
+            if (f.category) {
                 var category = f.category.split("/");
                 for (let i = 0; i < category.length; i++) {
-                    if(lastCategory[i]!==category[i]) {
-                        lastCategory.length = i+1;
-                        currentCategoryGroups.length = i+1
+                    if (lastCategory[i] !== category[i]) {
+                        lastCategory.length = i + 1;
+                        currentCategoryGroups.length = i + 1
 
                         lastCategory[i] = category[i];
-                        currentCategoryGroups[i] = new Category(category[i],fid==0);
-                        if(i==0) {
+                        currentCategoryGroups[i] = new Category(category[i], fid == 0);
+                        if (i == 0) {
                             createtpform.appendChild(currentCategoryGroups[i].getElement());
                         }
                         else {
-                            currentCategoryGroups[i-1].appendChild(currentCategoryGroups[i]);
+                            currentCategoryGroups[i - 1].appendChild(currentCategoryGroups[i]);
                         }
                         /*var categoryHeading = document.createElement('h'+(i+1));
                         categoryHeading.innerText = category[i];*/
                     }
                 }
-                currentCategoryGroups[currentCategoryGroups.length-1].appendChild(formItem)
+                currentCategoryGroups[currentCategoryGroups.length - 1].appendChild(formItem)
             } else {
                 createtpform.appendChild(formItem);
             }
@@ -135,7 +187,7 @@ document.addEventListener('DOMContentLoaded', () => {
         button.type = "submit";
         createtpform.appendChild(button);
 
-        
+
         var jsonbutton = document.createElement('a');
         jsonbutton.classList.add("btn", "btn-secondary");
         jsonbutton.innerHTML = "Download One Click File";
